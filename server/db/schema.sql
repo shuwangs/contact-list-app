1d236c56da074241adfa36c82f866848a35755de
shu.wang <60482034+shuwangs@users.noreply.github.com>
2026-03-17 22:03:19 +0000

    update schema

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -21,15 +21,3 @@ CREATE TABLE contacts(
     is_emergency_contact BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMP DEFAULT NOW()
 );
-
--- insert user
-INSERT INTO users (name, email)
-VALUES ('bobo', 'bobo@example.com');
-VALUES ('tester1', 'tester1@example.com');
-
--- add contacts to user 1
-INSERT INTO contacts (user_id, first_name, last_name, phone_number, email, notes)
-VALUES
-(1, 'Alice', 'Johnson', '2021112222', 'alice@example.com', 'Friend from Techtonica'),
-(1, 'Bob', 'Lee', '7033334444', 'bob@example.com', 'Emergency contact'),
-(1, 'Cathy', 'Chen', NULL, 'cathy@example.com', 'Met at networking event');
