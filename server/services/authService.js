0ab7c42ba5d009f1462fd6a098ee02aea31a5142
shu.wang <shuw425@gmail.com>
2026-03-18 02:04:53 -0400

    add authService - add getUserByEmail - register user - logic to determine wither it is existing user or create a newUser

--- server/services/authService.js
+++ server/services/authService.js
@@ -1,9 +1,9 @@
 
-import {pool } from '../db/db.js';
+import pool  from '../db/db.js';
 
 export const registerUser = async (name, email) => {
     const { rows } = await pool.query(
-        `INSERT INTO users(name, email) VALUES ($1, $2) RETURNING id`,
+        `INSERT INTO contact_app.users(name, email) VALUES ($1, $2) RETURNING id`,
         [name, email]
     );
     console.log(rows);
@@ -12,11 +12,20 @@ export const registerUser = async (name, email) => {
 
 export const getUserByEmail = async (email) => {
     const { rows } = await pool.query(
-        `SELECT * FROM users WHERE email = $1`,
+        `SELECT * FROM contact_app.users WHERE email = $1`,
         [email]
     );
     console.log(rows);
     return rows[0];
 };
 
-        
+export const findOrCreateUser = async (email) => {
+    const existingUser = await getUserByEmail(email);
+    if ( existingUser) {
+        console.log( "User exist",  existingUser );
+        return existingUser;
+    }
+    const newUser = await registerUser(name, email);
+    console.log('New user created: ', newUser);
+    return newUser;
+}
