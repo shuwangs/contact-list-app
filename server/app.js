34b5095ede7c0202659e70beab7dd55bd9125241
shu.wang <shuw425@gmail.com>
2026-03-25 18:25:23 -0400

    feat: add authMiddleware for request verification

--- server/app.js
+++ server/app.js
@@ -3,14 +3,14 @@ import express from "express";
 import authRoute from "./routes/authRoute.js";
 import userRoute from "./routes/userRoute.js";
 import contactRoute from "./routes/contactRoute.js";
-
+import authenticateToken from "./middleware/authMiddleware.js";
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/contacts", contactRoute);
+app.use("/api/users", authenticateToken, userRoute);
+app.use("/api/contacts", authenticateToken, contactRoute);
 
 export default app;
